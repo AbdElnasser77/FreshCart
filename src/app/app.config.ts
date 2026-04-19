d3502bc391e52d9e81289ecfd612c0f64307e8a2
AbdElnasser77 <a7med3bnaser77@gmail.com>
2026-04-19 02:45:14 +0200

    changed register UI to primeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,26 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import aura from '@primeng/themes/aura';
+import { MessageService } from 'primeng/api';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes,withViewTransitions()), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideRouter(routes, withViewTransitions()), provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+    providePrimeNG({
+      ripple: true,
+      theme:{
+        preset:aura,
+        options:{
+          darkModeSelector: '.dark'
+        }
+      }
+    }),
+    MessageService,
   ]
 };
--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,10 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
+import { AbstractControl, FormBuilder, FormControl,FormsModule, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { ClientSideValidation } from "../../components/client-side-validation/client-side-validation";
 import { AuthService } from '../../services/auth-service';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageService } from 'primeng/api';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, ClientSideValidation],
+  imports: [FormsModule,FloatLabelModule,ReactiveFormsModule, ClientSideValidation, MessageModule, ToastModule, ButtonModule, InputTextModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
@@ -12,27 +19,40 @@ export class Register {
 
   private readonly fb = inject(FormBuilder);
   private readonly authService = inject(AuthService);
-  registerForm = this.fb.group({
-    name:['',[Validators.required,Validators.minLength(2),Validators.maxLength(20)]],
-    email:['', [Validators.required,Validators.email]],
-    password:['',[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
-    rePassword:['',[Validators.required]],
-  },{validators:this.passwordMatchValidator})
+  messageService = inject(MessageService);
+  formSubmitted: boolean = false;
+    value1: string | undefined;
+    value2: string | undefined;
+    value3: string | undefined;
 
-  passwordMatchValidator(control: AbstractControl){
-    return control.get('password')?.value == control.get('rePassword')?.value ? null : {missmatch : true};
+  registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
+    rePassword: ['', [Validators.required]],
+  }, { validators: this.passwordMatchValidator })
+
+  passwordMatchValidator(control: AbstractControl) {
+    return control.get('password')?.value == control.get('rePassword')?.value ? null : { missmatch: true };
   }
-  register(){
-    if(this.registerForm.valid){
+  register() {
+      this.formSubmitted = true;
+    if (this.registerForm.valid) {
       console.log(this.registerForm.value);
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submitted', life: 3000 });
+      this.registerForm.reset();
+      this.formSubmitted = false;
       this.authService.SignUp(this.registerForm.getRawValue()).subscribe({
-        next:(res)=>{
+        next: (res) => {
           console.log(res);
-          localStorage.setItem('token',res.token);
+          localStorage.setItem('token', res.token);
         }
       })
-    }else{
-      this.registerForm.markAllAsTouched();
     }
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.registerForm.get(controlName);
+    return control?.invalid && (control.touched || this.formSubmitted);
   }
 }
